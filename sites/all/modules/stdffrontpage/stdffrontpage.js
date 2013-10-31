e31b41c92025b52b04965dc73367b816d8918a96
Rob Wild <robert@appnovation.com>
2013-10-31 16:02:47 +0000

    Fix for https://www.pivotaltracker.com/s/projects/838407/stories/58668624 Adjusted js output hijack of node queue to warp disary graphic into something controllable with CSS, dropped the image and replaced with CSS, adjusted styling as per ticket request

--- sites/all/modules/stdffrontpage/stdffrontpage.js
+++ sites/all/modules/stdffrontpage/stdffrontpage.js
@@ -10,19 +10,18 @@
 				$('.node-event').each(function(index) {
 				  var raw = $(this).find('.date-display-start').html();
 			      var date = raw.split(" ");
-			      $(this).parent().prepend('<span class="month">' + date[1].substr(0,3) + '</span>');
-			      $(this).parent().prepend('<span class="day">' + date[0] + '</span>');
+			      $(this).parent().prepend('<div class="date"><span class="day">' + date[0] + '</span><span class="month">' + date[1].substr(0,3) + '</span></div>');
 				});	
 				//This code adds a class to colour the status field value for the project table.
 				$('td.views-field-field-status').each(function(index) {	
 				  $(this).addClass($(this).text().toLowerCase());
 				});	
-				$('#block-stdffrontpage-publications > ul > li > a').append('<img title="More Info" src="sites/all/themes/wto-stdf/images/linkarrow.png" />');
-				$('.node-event > header > h2 > a').append('<img title="More Info" src="sites/all/themes/wto-stdf/images/linkarrow.png" />');
+				$('#block-stdffrontpage-publications > ul > li > a').append('<img src="sites/all/themes/wto-stdf/images/linkarrow.png" />');
+				$('.node-event > header > h2 > a').append('<img src="sites/all/themes/wto-stdf/images/linkarrow.png" />');
 			    var event = $('#block-views-nodequeue-upcoming-events-block > h2');
-			    event.append('<img title="Upcoming Events" src="sites/all/themes/wto-stdf/images/eventsicon_bg.png" />');
+			    event.append('<img src="sites/all/themes/wto-stdf/images/eventsicon_bg.png" />');
 			    var pub = $('#block-stdffrontpage-publications > h2');
-			    pub.append('<img title="Publications" src="sites/all/themes/wto-stdf/images/pubicon_bg.png" />');
+			    pub.append('<img src="sites/all/themes/wto-stdf/images/pubicon_bg.png" />');
 			    
 				}
 			    
